09f14dd1cf2f61b8b4e9a2c04dc9bff9ef382fb3
NishaJahir <49142761+NishaJahir@users.noreply.github.com>
2022-02-14 18:32:40 +0530

    Update PaymentService.php

--- src/Services/PaymentService.php
+++ src/Services/PaymentService.php
@@ -825,15 +825,21 @@ class PaymentService
          } else {
              $invoiceAmount = (float) $order->amounts[0]->invoiceTotal;
          }
-         
-            
+          
+         // Get the updated payment details
+         foreach ($paymentDetails as $paymentDetail)
+         {
+            $paymentCurrency = $paymentDetail->currency;
+            $paymentMopId = $paymentDetail->mopId;
+         }
+
          if ($responseData['status'] == '100') {
-            $paymentData['currency']    = $paymentDetails[0]->currency;
+            $paymentData['currency']    = $paymentCurrency;
             $paymentData['paid_amount'] = (float) $invoiceAmount;
             $paymentData['tid']         = $tid;
             $paymentData['order_no']    = $order->id;
             $paymentData['type']        = $responseData['tid_status'] != '100' ? 'cancel' : 'credit';
-            $paymentData['mop']         = $paymentDetails[0]->mopId;
+            $paymentData['mop']         = $paymentMopId;
             $paymentData['tid_status']  = $responseData['tid_status'];
             
             $orderDetails = $this->transactionLogData->updateTransactionData('orderNo', $order->id, $responseData['tid_status']);
