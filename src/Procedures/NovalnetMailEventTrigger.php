488b435e1421e8f8164966187962b51b9aacd78b
NishaJahir <49142761+NishaJahir@users.noreply.github.com>
2022-02-21 19:13:28 +0530

    Update and rename MailEventTrigger.php to NovalnetMailEventTrigger.php

---
+++ src/Procedures/NovalnetMailEventTrigger.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * This module is used for real time processing of
+ * Novalnet payment module of customers.
+ * This free contribution made by request.
+ * 
+ * If you have found this script useful a small
+ * recommendation as well as a comment on merchant form
+ * would be greatly appreciated.
+ *
+ * @author       Novalnet AG
+ * @copyright(C) Novalnet
+ * All rights reserved. https://www.novalnet.de/payment-plugins/kostenlos/lizenz
+ */
+ 
+namespace Novalnet\Procedures;
+
+use Plenty\Modules\EventProcedures\Events\EventProceduresTriggered;
+use Plenty\Modules\Order\Models\Order;
+use Plenty\Plugin\Log\Loggable;
+use Plenty\Modules\Payment\Contracts\PaymentRepositoryContract;
+use Novalnet\Services\PaymentService;
+use Novalnet\Services\TransactionService;
+
+/**
+ * Class NovalnetMailEventTrigger
+ */
+class NovalnetMailEventTrigger
+{
+    use Loggable;
+    
+    $this->getLogger(__METHOD__)->error('call', 'NISHU');
+}
