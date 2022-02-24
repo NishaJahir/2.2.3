b84e0e7f90352cec7c8fe305b8e7043ca81bb130
NishaJahir <49142761+NishaJahir@users.noreply.github.com>
2022-02-24 16:50:38 +0530

    Update NovalnetMailEventTrigger.php

--- src/Procedures/NovalnetMailEventTrigger.php
+++ src/Procedures/NovalnetMailEventTrigger.php
@@ -66,17 +66,18 @@ class NovalnetMailEventTrigger
         /* @var $order Order */
      
         $order = $eventTriggered->getOrder(); 
-     $this->getLogger(__METHOD__)->error('payment created 12345', $order);
+        $this->getLogger(__METHOD__)->error('payment created 12345', $order);
         $payments = pluginApp(\Plenty\Modules\Payment\Contracts\PaymentRepositoryContract::class);  
         $paymentDetails = $payments->getPaymentsByOrderId($order->id);
         $this->getLogger(__METHOD__)->error('payment created 123', $paymentDetails);
-     $this->getLogger(__METHOD__)->error('payment created count', count($paymentDetails));
+        $this->getLogger(__METHOD__)->error('payment created count', count($paymentDetails));
      
         if(count($paymentDetails) == 1)  {
            $this->getLogger(__METHOD__)->error('payment created', $paymentDetails);
-           $eventService->fireTrigger($order->id, 'Novalnet', 'NovalnetMailEventTrigger');
+           return true;
         } else {
            $this->getLogger(__METHOD__)->error('payment not created', $paymentDetails);
+           return false;
         }
     }
 }
